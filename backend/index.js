2543a10f795994054cc7609becb41a95fb8f9b49
shidong.ke <shidong.ke@shopee.com>
2020-09-28 16:34:09 +0800

    fix: BUG

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,55 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const webpush = require('web-push')
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 
-const port = 4000;
+const port = 4000
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'))
 
-const dummyDb = { subscription: null }; //dummy in memory store
+const dummyDb = { subscription: null } //dummy in memory store
 
 const saveToDatabase = async subscription => {
   // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
   // Here you should be writing your db logic to save it.
-  dummyDb.subscription = subscription;
-};
+  dummyDb.subscription = subscription
+}
 
 // The new /save-subscription endpoint
-app.post("/save-subscription", async (req, res) => {
-  const subscription = req.body;
-  await saveToDatabase(subscription); //Method to save the subscription to Database
-  res.json({ message: "success" });
-});
+app.post('/save-subscription', async (req, res) => {
+  const subscription = req.body
+  await saveToDatabase(subscription) //Method to save the subscription to Database
+  res.json({ message: 'success' })
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const vapidKeys = {
+  publicKey:
+    'BIMt90ZkpsUhsq9uGbSoKgy-LgQPbL3X9qtKjxOvdhVvJf586wwGGV7nFBdFPGLFYG68hW5zHBGTdeQsmpeiHW8',
+  privateKey: 'L2YQQTyJnmUPjsfh4SOqDsbb9uZVoi-aOPezOR-RcLY',
+}
+
+//setting our previously generated VAPID keys
+webpush.setVapidDetails(
+  'mailto:myuserid@email.com',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+)
+
+//function to send the notification to the subscribed device
+const sendNotification = (subscription, dataToSend) => {
+  webpush.sendNotification(subscription, dataToSend)
+}
+
+//route to test send notification
+app.get('/send-notification', (req, res) => {
+  const subscription = dummyDb.subscription //get subscription from your databse here.
+  const message = 'Hello World'
+  sendNotification(subscription, message)
+  res.json({ message: 'message sent' })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
